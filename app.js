93a6977302f71df4aabc705c664651ae1c2937b3
Fabian Deneuville <deneuville.fabian@live.fr>
2022-03-21 16:31:23 +0100

    Adding specific access control headers to prevent CORS errors

--- app.js
+++ app.js
@@ -21,5 +21,13 @@ mongoose.connect(`mongodb+srv://${dbUserName}:${dbPassword}@cluster0.12gfd.mongo
 /* Creating the express app using the express method */
 const app = express();
 
+/* Specifying specific access control headers for all the response objects to allow cross-origin requests (and prevent CORS errors) */
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 /* Exporting the express app to be used on other files */
 module.exports = app;
