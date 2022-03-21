7b8716a372e30eb497e071beb8c90a4f9bd48437
Fabian Deneuville <deneuville.fabian@live.fr>
2022-03-21 16:16:38 +0100

    connecting express application to the MongoDB database

--- app.js
+++ app.js
@@ -3,6 +3,21 @@
 /* Importing express */
 const express = require('express');
 
+/* Importing mongoose */
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const dbUserName = process.env.dbUserName;
+const dbPassword = process.env.dbPassword;
+
+/* Conncting the express application to the MongoDB database */
+mongoose.connect(`mongodb+srv://${dbUserName}:${dbPassword}@cluster0.12gfd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 /* Creating the express app using the express method */
 const app = express();
 
