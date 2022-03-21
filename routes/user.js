5ce14928806801f0e2b3c2e21bf4a80806934b68
Fabian Deneuville <deneuville.fabian@live.fr>
2022-03-21 19:57:51 +0100

    Creating user router

---
+++ routes/user.js
@@ -0,0 +1,17 @@
+/******************** USER ROOTER CONFIGURATION ********************/
+
+/* Importing express */
+const express = require('express');
+
+/* Creating router with the Router() function */
+const router = express.Router();
+
+/* Importing user controller */
+const userControl = require('../controllers/user');
+
+/* Creating the signup and login routes */
+router.post('/signup', userControl.signup);
+router.post('/login', userControl.login)
+
+/* Exporting router */
+module.exports = router;
