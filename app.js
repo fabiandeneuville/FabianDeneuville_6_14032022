5f3f212b30c29f79073a01eedfed7ee75a8e6834
Fabian Deneuville <deneuville.fabian@live.fr>
2022-03-21 15:20:27 +0100

    Creating express application

---
+++ app.js
@@ -0,0 +1,10 @@
+/******************** EXPRESS APPLICATION CONFIGURATION ********************/
+
+/* Importing express */
+const express = require('express');
+
+/* Creating the express app using the express method */
+const app = express();
+
+/* Exporting the express app to be used on other files */
+module.exports = app;
