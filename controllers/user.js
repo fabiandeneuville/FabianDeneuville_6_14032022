8c5258f5c082331ee75cac8cb30e123b3e5c041f
Fabian Deneuville <deneuville.fabian@live.fr>
2022-03-22 09:35:08 +0100

    Adding token configuration with jsonwebtoken

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,9 @@
 /* Importing bcrypt */
 const bcrypt = require('bcrypt');
 
+/* Importing jsonwebtoken */
+const jwt = require('jsonwebtoken')
+
 /* Importing User model */
 const User = require('../models/User');
 
@@ -35,7 +38,11 @@ exports.login = (req, res, next) => {
             }
             res.status(200).json({
                 userId: user._id,
-                token: 'TOKEN'
+                token: jwt.sign(
+                    {userId: user._id},
+                    'RANDOM_TOKEN_SECRET',
+                    {expiresIn: '24h'}
+                )
             })
         })
         .catch(error => res.status(500).json({error}))
