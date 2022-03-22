2f0c9d1710dd3e23f717116c835cb7185e619db4
Fabian Deneuville <deneuville.fabian@live.fr>
2022-03-22 22:16:18 +0100

    Adding the sauce controller

---
+++ controllers/sauce.js
@@ -0,0 +1,53 @@
+/******************** SAUCE CONTROLLER CONFIGURATION ********************/
+
+const Sauce = require('../models/Sauce');
+
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    const sauce = new Sauce({
+      ...sauceObject,
+      likes:0,
+      dislikes:0,
+      userDisliked: [],
+      userLiked: [],
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    })
+    sauce.save()
+      .then(() => res.status(201).json({message: 'Sauce ajoutée'}))
+      .catch(error => res.status(400).json({error}));
+  };
+
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(404).json({error}))
+  };
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({_id: req.params.id})
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({error}));
+  };
+
+exports.modifySauce = (req, res, next) => {
+    Sauce.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+    .then(() => res.status(200).json({message: 'Sauce modifiée'}))
+    .catch(error => res.status(400).json({error}));
+  };
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({_id:req.params.id})
+    .then((sauce) => {
+        if(!sauce){
+            return res.status(404).json({error});
+        }
+        if(sauce.userId !== req.auth.userId){
+            return res.status(401).json({error});
+        }
+        Sauce.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json({message: 'Sauce supprimée'}))
+        .catch(error => res.status(400).json({error}))
+    })
+};
+
