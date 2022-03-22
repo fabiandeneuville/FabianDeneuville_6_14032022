572a9615d5fae92426f224625e567f230446aaa3
Fabian Deneuville <deneuville.fabian@live.fr>
2022-03-22 18:57:18 +0100

    Adding Sauce model

---
+++ models/sauce.js
@@ -0,0 +1,22 @@
+/******************** SAUCE MODEL CONFIGURATION ********************/
+
+/* Importing express */
+const express = require('mongoose');
+
+/* Creating sauce schema */
+const sauceSchema = mongoose.Schema({
+    userId: {type: String, required: true},
+    name: {type: String, required: true},
+    manufacturer: {type: String, required: true},
+    description: {type: String, required: true},
+    mainPepper: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    heat: {type: Number, required: true},
+    likes: {type: Number, required: true},
+    dislikes: {type: Number, required: true},
+    usersLiked: {type: Array, required: true},
+    usersDisliked: {type: Array, required: true} 
+})
+
+/* Exporting sauce schema as model */
+module.exports = mongoose.model('Sauce', sauceSchema);
