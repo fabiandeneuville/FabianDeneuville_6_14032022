53c6be65a8c03be31949ff101528c9bf7941183a
Fabian Deneuville <deneuville.fabian@live.fr>
2022-03-23 09:36:04 +0100

    Modifying Delete function to remove the sauce image from the images folder when the sauce is deleted

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -3,6 +3,9 @@
 /* Importing Sauce model */
 const Sauce = require('../models/Sauce');
 
+/* Importing the Node File System package */
+const fs = require('fs');
+
 /* creating the function to POST a new sauce */
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
@@ -48,17 +51,14 @@ exports.modifySauce = (req, res, next) => {
 
 /* Creating the function to DELETE one specific sauce */
 exports.deleteSauce = (req, res, next) => {
-    Sauce.findOne({_id:req.params.id})
-    .then((sauce) => {
-        if(!sauce){
-            return res.status(404).json({error});
-        }
-        if(sauce.userId !== req.auth.userId){
-            return res.status(401).json({error});
-        }
+    Sauce.findOne({_id: req.params.id})
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images')[1];
+      fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({_id: req.params.id})
         .then(() => res.status(200).json({message: 'Sauce supprimée'}))
         .catch(error => res.status(400).json({error}))
+      })
     })
+    .catch(error => res.status(500).json({error}));
 };
-
