1310cb150a08e31041f1f316b7c2f3d362840ca4
Fabian Deneuville <deneuville.fabian@live.fr>
2022-03-23 09:05:22 +0100

    Adding comments

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,7 +1,9 @@
 /******************** SAUCE CONTROLLER CONFIGURATION ********************/
 
+/* Importing Sauce model */
 const Sauce = require('../models/Sauce');
 
+/* creating the function to POST a new sauce */
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
@@ -18,24 +20,28 @@ exports.createSauce = (req, res, next) => {
       .catch(error => res.status(400).json({error}));
   };
 
+/* Creating the function to GET all sauces */
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(404).json({error}))
   };
 
+/* Creating the function to GET a specific sauce */
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({_id: req.params.id})
     .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json({error}));
   };
 
+/* Creating the function to modify (PUT) one specific sauce */
 exports.modifySauce = (req, res, next) => {
     Sauce.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
     .then(() => res.status(200).json({message: 'Sauce modifiée'}))
     .catch(error => res.status(400).json({error}));
   };
 
+/* Creating the function to DELETE one specific sauce */
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({_id:req.params.id})
     .then((sauce) => {
